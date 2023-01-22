eb9f93db7c221a98564cfe4d9b0acd662d52f0b8
Aditya <79000241+aditya09iconic@users.noreply.github.com>
2023-01-22 15:02:15 +0530

    Add files via upload

---
+++ part11/open pokedex/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const process = require('process');
+
+const app = express();
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('dist'));
+
+app.get('/health', (_, res) => {
+  res.send('ok');
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000');
+});
